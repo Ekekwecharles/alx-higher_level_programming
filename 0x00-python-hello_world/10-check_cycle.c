d6a7a0fb799ce4e878b4e03252ec1f57171d29b1
Charlessnow <ekekwecharles50@gmail.com>
2023-05-08 19:20:23 -0700

    linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,6 +8,9 @@ int check_cycle(listint_t *list)
 {
 	listint_t *tortoise = list;
 	listint_t *hare = list->next;
+
+	if (!list)
+		return (0);
 	/* if tortoise == to hare */
 	while (tortoise && hare && hare->next)
 	{
