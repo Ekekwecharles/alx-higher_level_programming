82b414e089a6d8b43e7e7432f7724527ad4658fb
Charlessnow <ekekwecharles50@gmail.com>
2023-05-09 17:10:00 -0700

    updated task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,9 +8,10 @@
  * Return: addressof the added node
  *	otherwise NULL
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new, *prev;
+	listint_t *node = *head, *new, *prev = NULL;
 
 	if (!head)
 		return (NULL);
@@ -29,11 +30,65 @@ listint_t *insert_node(listint_t **head, int number)
 			node = node->next;
 			continue;
 		}
-		new->next = prev->next;
-		prev->next = new;
+		else if (!prev)
+		{
+			new->next = *head;
+			*head = new;
+		}
+		else
+		{
+			new->next = prev->next;
+			prev->next = new;
+		}
 		return (new);
 	}
 
+	if (!prev)
+		*head = new;
+	else
+		prev->next = new;
+
 	return (new);
 }
+#if 0
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temp, *new_node, *prev = NULL;
 
+	if (!head)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = number;
+	new_node->next = NULL;
+
+	temp = *head;
+	while (temp && new_node->n > temp->n)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+	/* for duplicate node values */
+	if (temp && new_node->n == temp->n)
+	{
+		new_node->next = temp;
+		prev->next = new_node;
+	}
+	/* when new_node value is lower than value at first node */
+	else if (!prev)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		new_node->next = prev->next;
+		prev->next = new_node;
+	}
+
+	return (new_node);
+}
+#endif
--- 0x01-python-if_else_loops_functions/13-main.c
+++ 0x01-python-if_else_loops_functions/13-main.c
@@ -8,6 +8,7 @@
  *
  * Return: Always 0.
  */
+#if 0
 int main(void)
 {
     listint_t *head;
@@ -32,4 +33,112 @@ int main(void)
     free_listint(head);
 
     return (0);
+}
+#endif
+
+#if 0
+int main(void)
+{
+	listint_t *head;
+
+	head = NULL;
+	print_listint(head);
+
+	printf("-----------------\n");
+
+	insert_node(&head, 972);
+
+	print_listint(head);
+
+	free_listint(head);
+
+	return (0);
+}
+#endif
+
+#if 0
+int main(void)
+{
+	listint_t *head;
+
+	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
+	print_listint(head);
+
+	printf("-----------------\n");
+
+	insert_node(&head, -7);
+
+	print_listint(head);
+
+	free_listint(head);
+
+	return (0);
+}
+#endif
+
+#if 0
+int main(void)
+{
+	listint_t *head;
+
+	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
+	print_listint(head);
+
+	printf("-----------------\n");
+
+	insert_node(&head, 5727);
+
+	print_listint(head);
+
+	free_listint(head);
+
+	return (0);
+}
+#endif
+
+int main(void)
+{
+	listint_t *head;
+
+	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
+	print_listint(head);
+
+	printf("-----------------\n");
+
+	insert_node(&head, 5);
+	insert_node(&head, -32);
+	insert_node(&head, 5432);
+	insert_node(&head, 101);
+	insert_node(&head, 47);
+	insert_node(&head, 6405);
+
+	print_listint(head);
+
+	free_listint(head);
+
+	return (0);
 }
