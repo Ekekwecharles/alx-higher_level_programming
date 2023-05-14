334c4b94e68bda83d3b2f1e577fc592130af3f27
Charlessnow <ekekwecharles50@gmail.com>
2023-05-14 07:47:26 -0700

    python scripts

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        sentence += None
-        return 0, sentence[0]
-    j = 0
-    for i in sentence:
-        j += 1
-    return j, sentence[0]
+        return None, None
+    return len(sentence), sentence[0]
