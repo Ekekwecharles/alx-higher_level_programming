cc4ab3eed41158a3c19c807bacb3d3d906d7d25e
Charlessnow <ekekwecharles50@gmail.com>
2023-06-07 03:45:18 -0700

    updated 2-matrix_divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,25 +2,25 @@
 """Matrix division module"""
 
 
-def matrix_divided(matrix=[[1]], div=1):
+def matrix_divided(matrix, div):
     """Divides each element of the 2 dimensional matrix"""
-    if type(div) is not int and type(div) is not float:
-        raise TypeError("div must be a number")
+    if not isinstance(div, (int, float)):
+        raise TypeError('div must be a number')
     if div == 0:
-        raise ZeroDivisionError("division by zero")
-    if type(matrix) is not list or (len(matrix) == 0) or \
-            type(matrix[0]) is not list or (len(matrix[0]) == 0):
-        raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
-    for row in matrix:
-        if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        for c in row:
-            if type(c) is not int and type(c) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
+        raise ZeroDivisionError('division by zero')
+    if not matrix or matrix == [] \
+            or not all(isinstance(row, list) for row in matrix):
+        raise TypeError('matrix must be a matrix '
+                        '(list of lists) of integers/floats')
+    if not all(isinstance(elem, (int, float))
+               for row in matrix for elem in row):
+        raise TypeError('matrix must be a matrix '
+                        '(list of lists) of integers/floats')
 
-    return [[round(item / div, 2) for item in row] for row in matrix]
+    row_len = len(matrix[0])
+    if not all(len(row) == row_len for row in matrix):
+        raise TypeError('Each row of the matrix must have the same size')
+
+    new_matrix = [[round(elem / div, 2) for elem in row] for row in matrix]
+
+    return new_matrix
