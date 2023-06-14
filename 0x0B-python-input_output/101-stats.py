783622ac8816e772806c581ffe77365d41769eea
Charlessnow <ekekwecharles50@gmail.com>
2023-06-14 06:44:15 -0700

    log parser

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,34 +1,49 @@
 #!/usr/bin/python3
 """script that reads stdin line by line and computes metrics
 """
-import sys
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 1
 
-try:
-    for line in sys.stdin:
-        # Parse each line of the input
-        parts = line.split()
-        status_code = int(parts[-2])
-        file_size = int(parts[-1])
-
-        # Update metrics
-        total_size += file_size
-        status_codes[status_code] += 1
-
-        # print statistics every 10 lines
-        if line_count % 10 == 0:
-            print("File size:", total_size)
-            for code in sorted(status_codes.keys()):
-                if status_codes[code] > 0:
-                    print(f"{code}: {status_codes[code]}")
-
-        line_count += 1
-except KeyboardInterrupt:
+def print_statistics(total_size, status_codes):
+    """print statistics every 10 lines"""
     print("File size:", total_size)
     for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
-    raise
+        print(f"{code}: {status_codes[code]}")
+
+
+if __name__ == "__main__":
+    import sys
+
+    total_size = 0
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    status_codes = {}
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            if line_count == 10:
+                print_statistics(total_size, status_codes)
+                line_count = 1
+            else:
+                line_count += 1
+
+            parts = line.split()
+
+            try:
+                total_size += int(parts[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if parts[-2] in valid_codes:
+                    """get returns -1 if key:value doesn't exist"""
+                    if status_codes.get(parts[-2], -1) == -1:
+                        status_codes[parts[-2]] = 1
+                    else:
+                        status_codes[parts[-2]] += 1
+            except IndexError:
+                pass
+        print_statistics(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_statistics(total_size, status_codes)
+        raise
