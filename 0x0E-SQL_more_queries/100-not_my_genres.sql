2fb1e5263b701290d809676405fe67eaa1e899c5
Charlessnow <ekekwecharles50@gmail.com>
2023-07-19 23:46:53 -0700

    sql queries

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,4 @@
---- Write a script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+-- Write a script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
 
 SELECT tv_genres.name
 FROM tv_genres
