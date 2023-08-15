0fb9a5f7abaeca14da630f4e064e726ceec6c41a
Charlessnow <ekekwecharles50@gmail.com>
2023-08-15 15:11:39 -0700

    Fix task 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -5,12 +5,3 @@ exports.converter = function (base) {
     return number.toString(base);
   };
 };
-
-
-
-
-
-12 divided by 2 is 6 with a remainder of 0
- 6 divided by 2 is 3 with a remainder of 0
- 3 divided by 2 is 1 with a remainder of 1
- 1 divided by 2 is 0 with a remainder of 1
